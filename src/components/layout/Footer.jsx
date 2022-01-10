babe3315863392a4e0757fe910a2ebe8c17ca088
babinicz <babinicz@onet.pl>
2022-01-10 14:24:12 +0100

    zm

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,18 +6,18 @@ import {
 
 export const Footer = () => {
   const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "Styczeń",
+    "Luty",
+    "Marzec",
+    "Kwiecień",
+    "Maj",
+    "Czerwiec",
+    "Lipiec",
+    "Sierpień",
+    "Wrzesień",
+    "Październik",
+    "Listopad",
+    "Grudzień",
   ];
 
   return (
