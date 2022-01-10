e34672e486f3785ac4fba6d6ea3ede7131853a54
babinicz <babinicz@onet.pl>
2022-01-10 12:29:56 +0100

    dodałem sitemap

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { ProjectsSection } from "./components/sections/ProjectsSection";
 import { ServicesSection } from "./components/styles/sections/ServicesSection";
 import { ContactSection } from "./components/sections/ContactSection.jsx";
 import { Footer } from "./components/layout/Footer";
+import CookieConsent from "react-cookie-consent";
 
 const App = () => {
   const theme = {
@@ -41,6 +42,33 @@ const App = () => {
         </Container>
         <Footer />
       </ThemeProvider>
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </>
   );
 };
